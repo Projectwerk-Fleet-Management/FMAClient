830ce107970c8b7f2197871004b9fad748eebbe5
BatselierBryan <bryan.batselier@student.hogent.be>
2021-10-09 11:23:36 +0200

    Beginning Driver class
    Added;
    - DriverException (Done)
    - Address (Done)
    - Driver (Half way done)

--- FMA Client/BusinessLayer/Model/Address.cs
+++ FMA Client/BusinessLayer/Model/Address.cs
@@ -2,6 +2,26 @@
 {
     public class Address
     {
-        
+        public string Street { get; private set; }
+        public string Housenumber { get; private set; }
+        public string Addendum { get; private set; }
+        public string City { get; private set; }
+        public string PostalCode { get; private set; }
+
+        public Address(string street, string housenumber, string city, string postalCode)
+        {
+            Street = street;
+            Housenumber = housenumber;
+            City = city;
+            PostalCode = postalCode;
+        }
+        public Address(string street, string housenumber, string addendum, string city, string postalCode)
+        {
+            Street = street;
+            Housenumber = housenumber;
+            Addendum = addendum;
+            City = city;
+            PostalCode = postalCode;
+        }
     }
 }
