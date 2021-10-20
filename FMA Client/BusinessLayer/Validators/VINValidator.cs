9209d0c0879023190fa42f92a534878570459417
BatselierBryan <bryan.batselier@student.hogent.be>
2021-10-20 11:32:05 +0200

    Finished VINValidator
    Also added some testing cases in FMA Client program

--- FMA Client/BusinessLayer/Validators/VINValidator.cs
+++ FMA Client/BusinessLayer/Validators/VINValidator.cs
@@ -6,20 +6,30 @@ namespace BusinessLayer.Validators
 {
     public class VINValidator
     {
-        private int _VINLength = 17; // VIN have a length of 17 (used to be 11 before 1981 but that's not the case anymore)
-        private int sum = 0;
-        private int checkDigit;
+        private int _VINLength = 17; // VIN has a length of 17 (used to be 11 before 1981 but that's not the case anymore)       
 
         public bool IsValid(string vin)
         {
-            vin.ToLower();
+            int sum = 0;
+            int checkDigit = 0;
+            string allowedLetters = "ABCDEFGHJKLMNPRSTUVWXYZ0123456789";
             if (vin == null) throw new VINValidatorException("VINValidator - VIN is empty");
             if (vin.Length != _VINLength) throw new VINValidatorException($"VINValidator - VIN length isn't equal to {_VINLength}");
-            if (vin.Contains("i") || vin.Contains("o") || vin.Contains("q")) throw new VINValidatorException("VINValidator - VIN contains an invalid character (I/O/Q)");
-            
-            Dictionary<char, int> charReplacerList = new Dictionary<char, int>();
 
-            #region hashtable insertion
+            foreach (char c in vin)
+            {
+                if (!allowedLetters.Contains(c))
+                {
+                    throw new VINValidatorException("VINValidator - VIN contains an invalid character (I/O/Q/...)");
+                }
+            }
+
+            vin.ToUpper();
+
+            Dictionary<char, int> charReplacerList = new Dictionary<char, int>();
+            Dictionary<char, int> charReplacerListCheckDigit = new Dictionary<char, int>();
+
+            #region Dictionary normal replacer insertion
             charReplacerList.Add('A', 1);
             charReplacerList.Add('B', 2);
             charReplacerList.Add('C', 3);
@@ -54,22 +64,93 @@ namespace BusinessLayer.Validators
             charReplacerList.Add('9', 9);
             charReplacerList.Add('0', 0);
             #endregion
+            #region Dictionary replacer checkDigit insertion
+            charReplacerListCheckDigit.Add('0', 0);
+            charReplacerListCheckDigit.Add('1', 1);
+            charReplacerListCheckDigit.Add('2', 2);
+            charReplacerListCheckDigit.Add('3', 3);
+            charReplacerListCheckDigit.Add('4', 4);
+            charReplacerListCheckDigit.Add('5', 5);
+            charReplacerListCheckDigit.Add('6', 6);
+            charReplacerListCheckDigit.Add('7', 7);
+            charReplacerListCheckDigit.Add('8', 8);
+            charReplacerListCheckDigit.Add('9', 9);
+            charReplacerListCheckDigit.Add('X', 10);
+            #endregion
 
 
             for (int i = 0; i < vin.Length; i++)
             {
-                if (i == 8)
+                switch (i)
                 {
-                    checkDigit = vin[i];
-                    continue;
+                    case 0:
+                        sum += charReplacerList[vin[i]] * 8;
+                        continue;
+
+                    case 1:
+                        sum += charReplacerList[vin[i]] * 7;
+                        continue;
+
+                    case 2:
+                        sum += charReplacerList[vin[i]] * 6;
+                        continue;
+
+                    case 3:
+                        sum += charReplacerList[vin[i]] * 5;
+                        continue;
+
+                    case 4:
+                        sum += charReplacerList[vin[i]] * 4;
+                        continue;
+
+                    case 5:
+                        sum += charReplacerList[vin[i]] * 3;
+                        continue;
+
+                    case 6:
+                        sum += charReplacerList[vin[i]] * 2;
+                        continue;
+
+                    case 7:
+                        sum += charReplacerList[vin[i]] * 10;
+                        continue;
+
+                    case 8:
+                        checkDigit = charReplacerListCheckDigit[vin[i]];
+                        continue;
+
+                    case 9:
+                        sum += charReplacerList[vin[i]] * 9;
+                        continue;
+
+                    case 10:
+                        sum += charReplacerList[vin[i]] * 8;
+                        continue;
+
+                    case 11:
+                        sum += charReplacerList[vin[i]] * 7;
+                        continue;
+
+                    case 12:
+                        sum += charReplacerList[vin[i]] * 6;
+                        continue;
+
+                    case 13:
+                        sum += charReplacerList[vin[i]] * 5;
+                        continue;
+
+                    case 14:
+                        sum += charReplacerList[vin[i]] * 4;
+                        continue;
+
+                    case 15:
+                        sum += charReplacerList[vin[i]] * 3;
+                        continue;
+
+                    case 16:
+                        sum += charReplacerList[vin[i]] * 2;
+                        continue;
                 }
-
-                int sum =+ charReplacerList[vin[i]];
-            }
-
-            foreach (char c in vin)
-            {
-                int sum = +charReplacerList[c];
             }
 
             if (sum % 11 != checkDigit)
