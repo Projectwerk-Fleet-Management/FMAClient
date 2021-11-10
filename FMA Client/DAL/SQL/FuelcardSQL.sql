2af9de75e1378147fe3f166b49528fd1034a5fd2
BatselierBryan <bryan.batselier@student.hogent.be>
2021-11-10 14:59:11 +0100

    Changed client
    Added proper sql statements
    Added Reference from vinvalidator

--- FMA Client/DAL/SQL/FuelcardSQL.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE `fmadatabase`.`fuelcards` (
-`fuelcardId` INT NOT NULL AUTO_INCREMENT,
-  `cardnumber` VARCHAR(255) NOT NULL,
-  `expiryDate` DATE NOT NULL,
-  `pincode` INT NULL,
-  `fueltypes` VARCHAR(255) NULL,
-  `isActive` BINARY(1) NULL,
-  PRIMARY KEY (`fuelcardId`),
-  UNIQUE INDEX `cardnumber_UNIQUE` ( `cardnumber` ASC) VISIBLE);
